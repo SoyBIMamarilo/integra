9054a5ab0691c861f02fc0d1a5199d7767054004
Andres Lozano <andresfelipelg5@gmail.com>
2023-09-20 16:51:28 -0500

    Deleted node backend

--- backend/routes/item.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const itemControllers = require("../controllers/item-controllers");
-
-const router = express.Router();
-
-router.delete("/:itemId", itemControllers.deleteItem);
-
-module.exports = router;
