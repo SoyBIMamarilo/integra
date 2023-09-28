aebf9b9f9e717142bc589408214fd2a2b8312913
Luis Fernando Gomez Patiño (Amarilo) <luisf.gomez@amarilo.com>
2023-09-28 08:21:09 -0500

    remove changes link card image

--- frontend/src/components/card/link-card.js
+++ frontend/src/components/card/link-card.js
@@ -1,17 +1,14 @@
 import Link from "next/link";
 
-const LinkCard = ({ id, href, title, subtitle}) => {
+const LinkCard = ({ id, href, title, subtitle }) => {
   return (
     <Link
       id={id}
       href={href}
-      className="flex min-w-[12rem] overflow-hidden border-2 border-solid border-neutral-700 px-2 py-3 hover:bg-neutral-100"
+      className=" min-w-[12rem] overflow-hidden border-2 border-solid border-neutral-700 px-2 py-3 hover:bg-neutral-100"
     >
-      <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-20 md:rounded-none md:rounded-l-lg" src="https://picsum.photos/200" alt="sss"></img>
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <div className="text-base">{title}</div>
-        <div className="text-xs font-extralight">{subtitle}</div>
-      </div>
+      <div className="text-base">{title}</div>
+      <div className="text-xs font-extralight">{subtitle}</div>
     </Link>
   );
 };
