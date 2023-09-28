07e323a11313f68376d9c12d8b6b4a39886eb532
afrobles <af.robles10@outlook.com>
2023-09-28 10:56:11 -0500

    version fix

--- frontend/src/app/(main)/@index/(index)/historic-items/[project]/[template]/layout.js
+++ frontend/src/app/(main)/@index/(index)/historic-items/[project]/[template]/layout.js
@@ -1,8 +1,19 @@
-export default function Layout({ create, children, params }) {
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { headers, cookies } from "next/headers";
+
+import { supabaseOptions } from "@/util/supabase";
+
+export default async function Layout({ create, children, params }) {
+  const supabase = createServerComponentClient({ cookies }, supabaseOptions);
+  const { data, error } = await supabase
+    .from("plantilla_presupuesto")
+    .select()
+    .eq("id", params.template);
+  const temp = data[0];
   return (
     <>
       {create}
-      <div className="mt-1 font-semibold">Versión: {params.template}</div>
+      <div className="mt-1 font-semibold">Versión: {temp.version}</div>
       {children}
     </>
   );
