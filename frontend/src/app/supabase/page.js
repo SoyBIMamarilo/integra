4313abc37606afe9ad21b82b7a8c70dc5c2203e9
Andres Lozano <andresfelipelg5@gmail.com>
2023-10-26 10:36:02 -0500

    Added delete handler

--- frontend/src/app/supabase/page.js
+++ frontend/src/app/supabase/page.js
@@ -1,17 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 // import { cookies, headers } from "next/headers";
 import { Suspense } from "react";
-import Alert from "@/components/alert";
 
 import { supabaseOptions } from "@/util/supabase";
 import SuspenseComponent from "./testLoading";
 import TestComponent from "./TestComponent";
 
 export default async function OptionalSession(params) {
-  return (
-    <>
-      <Alert />
-      <TestComponent />
-    </>
-  );
+  return <></>;
 }
