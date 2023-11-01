0901163a970f3a6020aa64546676d3a8b6dc7534
Andres Lozano <andresfelipelg5@gmail.com>
2023-11-01 08:32:19 -0500

    Adjusted login route

--- frontend/src/app/(main)/@login/page.js
+++ frontend/src/app/(main)/@login/page.js
@@ -1,16 +1,34 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 
 import Input from "@/components/input";
 
 import LogoAmarilo from "@/public/amarilo-logo.png";
 
 export default function Auth() {
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
+  const formSubmitHandler = async (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    router.refresh();
+  };
+
   return (
     <div className="w-100 flex h-screen flex-wrap content-evenly justify-center">
       <div className="flex h-fit w-min flex-col gap-3">
         <h1 className=" p-0 text-6xl font-semibold">INTEGRA </h1>
-        <form className="flex flex-col" action="/api/auth/login" method="post">
+        <form className="flex flex-col" onSubmit={formSubmitHandler}>
           <Input
             type="email"
             element="input"
