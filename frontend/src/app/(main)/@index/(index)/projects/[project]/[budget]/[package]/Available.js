302332b88f11e94602877dd1b3ed7a14a0ade5f4
Andres Lozano <andresfelipelg5@gmail.com>
2023-12-11 17:00:15 -0500

    Se añade buscador para histórico paquetes

--- frontend/src/app/(main)/@index/(index)/projects/[project]/[budget]/[package]/Available.js
+++ frontend/src/app/(main)/@index/(index)/projects/[project]/[budget]/[package]/Available.js
@@ -1,6 +1,7 @@
 "use client";
 
 import AvailableItem from "./AvailableItem";
+import AvailableItemSearch from "./AvailableItemSearch";
 import Search from "./Search";
 
 const Available = ({
@@ -13,8 +14,8 @@ const Available = ({
     <div className="flex w-[30%] flex-col">
       <div className="mb-3">Projectos Disponibles</div>
       <Search setSearchItemsHandler={setSearchItemsHandler} />
-      {searchItems && <div>Test</div>}
-      {projects &&
+      {!searchItems &&
+        projects &&
         projects.map((project) => (
           <AvailableItem
             key={project.linea_id}
@@ -22,6 +23,17 @@ const Available = ({
             addItemsHandler={addItemsHandler}
           />
         ))}
+      {searchItems &&
+        searchItems
+          .filter((pr) => pr.parent_id == null)
+          .map((project) => (
+            <AvailableItemSearch
+              key={project.linea_id}
+              project={project}
+              addItemsHandler={addItemsHandler}
+              itemList={searchItems}
+            />
+          ))}
     </div>
   );
 };
--- frontend/src/app/(main)/@index/(index)/projects/[project]/[budget]/[package]/Search.js
+++ frontend/src/app/(main)/@index/(index)/projects/[project]/[budget]/[package]/Search.js
@@ -16,15 +16,18 @@ const Search = ({ setSearchItemsHandler }) => {
   };
 
   return (
-    <div className="mb-2 flex flex-col">
-      <div className="flex flex-row">
+    <div className="mb-2 flex flex-col gap-2 text-xs font-bold">
+      <div className="flex flex-row self-end">
         <label className="pr-4">Descripción:</label>
         <input
           ref={searchRef}
-          className="ml-2 box-border w-full border-none"
+          className="ml-2 box-border w-32 border-none bg-integra-background-light"
         ></input>
       </div>
-      <button onClick={searchHandler} className="flex-0 self-end">
+      <button
+        onClick={searchHandler}
+        className="flex-0 self-end rounded-lg border-2 border-solid border-integra-text	 bg-integra-confirm-main px-5 py-1 text-xs font-bold text-integra-text hover:bg-integra-confirm-focus"
+      >
         Buscar
       </button>
     </div>
