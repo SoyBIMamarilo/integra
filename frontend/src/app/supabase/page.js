ca3b55065716478cab7ab696c44c74ef3451cd78
Andres Lozano <andresfelipelg5@gmail.com>
2023-12-14 07:47:37 -0500

    Se ajusta página pruebas supabases que no pasaba checks vercel

--- frontend/src/app/supabase/page.js
+++ frontend/src/app/supabase/page.js
@@ -1,9 +1,9 @@
 export default async function OptionalSession(params) {
-  const res = await fetch("localhost:3000/api/search-historic", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
-  const open = await res.json();
-  console.log("TEST SUPABASE");
+  // const res = await fetch("localhost:3000/api/search-historic", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  // });
+  // const open = await res.json();
+  // console.log("TEST SUPABASE");
   return <div>Prueba Tailwind</div>;
 }
